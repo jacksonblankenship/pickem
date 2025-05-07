761a05a35a926edfcafd791841e7d7757797c1db
Jackson Blankenship <jacksblan@gmail.com>
2025-05-07 15:30:54 -0500

    test category wrapper, pick status badge and pick status icon

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -8,48 +8,25 @@
 // You should NOT make any changes in this file as it will be overwritten.
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
-// Import Routes
+import { Route as rootRouteImport } from './routes/__root'
+import { Route as PicksGroupIdYearWeekRouteImport } from './routes/picks.$groupId.$year.$week'
 
-import { Route as rootRoute } from './routes/__root'
-import { Route as PicksGroupIdYearWeekImport } from './routes/picks.$groupId.$year.$week'
-
-// Create/Update Routes
-
-const PicksGroupIdYearWeekRoute = PicksGroupIdYearWeekImport.update({
+const PicksGroupIdYearWeekRoute = PicksGroupIdYearWeekRouteImport.update({
   id: '/picks/$groupId/$year/$week',
   path: '/picks/$groupId/$year/$week',
-  getParentRoute: () => rootRoute,
+  getParentRoute: () => rootRouteImport,
 } as any)
-
-// Populate the FileRoutesByPath interface
-
-declare module '@tanstack/react-router' {
-  interface FileRoutesByPath {
-    '/picks/$groupId/$year/$week': {
-      id: '/picks/$groupId/$year/$week'
-      path: '/picks/$groupId/$year/$week'
-      fullPath: '/picks/$groupId/$year/$week'
-      preLoaderRoute: typeof PicksGroupIdYearWeekImport
-      parentRoute: typeof rootRoute
-    }
-  }
-}
-
-// Create and export the route tree
 
 export interface FileRoutesByFullPath {
   '/picks/$groupId/$year/$week': typeof PicksGroupIdYearWeekRoute
 }
-
 export interface FileRoutesByTo {
   '/picks/$groupId/$year/$week': typeof PicksGroupIdYearWeekRoute
 }
-
 export interface FileRoutesById {
-  __root__: typeof rootRoute
+  __root__: typeof rootRouteImport
   '/picks/$groupId/$year/$week': typeof PicksGroupIdYearWeekRoute
 }
-
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
   fullPaths: '/picks/$groupId/$year/$week'
@@ -58,31 +35,25 @@ export interface FileRouteTypes {
   id: '__root__' | '/picks/$groupId/$year/$week'
   fileRoutesById: FileRoutesById
 }
-
 export interface RootRouteChildren {
   PicksGroupIdYearWeekRoute: typeof PicksGroupIdYearWeekRoute
+}
+
+declare module '@tanstack/react-router' {
+  interface FileRoutesByPath {
+    '/picks/$groupId/$year/$week': {
+      id: '/picks/$groupId/$year/$week'
+      path: '/picks/$groupId/$year/$week'
+      fullPath: '/picks/$groupId/$year/$week'
+      preLoaderRoute: typeof PicksGroupIdYearWeekRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+  }
 }
 
 const rootRouteChildren: RootRouteChildren = {
   PicksGroupIdYearWeekRoute: PicksGroupIdYearWeekRoute,
 }
-
-export const routeTree = rootRoute
+export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
   ._addFileTypes<FileRouteTypes>()
-
-/* ROUTE_MANIFEST_START
-{
-  "routes": {
-    "__root__": {
-      "filePath": "__root.tsx",
-      "children": [
-        "/picks/$groupId/$year/$week"
-      ]
-    },
-    "/picks/$groupId/$year/$week": {
-      "filePath": "picks.$groupId.$year.$week.tsx"
-    }
-  }
-}
-ROUTE_MANIFEST_END */
