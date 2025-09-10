5f519150d98df30798c86dcc9fad6c3510fe0f82
Jackson Blankenship <jacksblan@gmail.com>
2025-09-10 17:47:02 -0500

    add label for when picks close

--- src/components/game-card/game-header.tsx
+++ src/components/game-card/game-header.tsx
@@ -1,16 +1,18 @@
+import { useHasGameStarted } from '@/hooks/use-has-game-started';
 import { PropsWithClassName } from '@/lib/types';
 import { cn } from '@/lib/utils';
 import { formatInTimeZone } from 'date-fns-tz';
+import { Lock } from 'lucide-react';
 import { useGameCardContext } from '../../context/game-card-context';
-
 type GameHeaderProps = PropsWithClassName;
 
 export function GameHeader(props: GameHeaderProps) {
   const { date } = useGameCardContext();
+  const hasGameStarted = useHasGameStarted(date);
 
   return (
     <div className={cn('flex items-center justify-between', props.className)}>
-      <p className="text-xs text-slate-600">
+      <p className="flex items-center gap-1 text-xs text-slate-600">
         {date === null
           ? 'TBD'
           : formatInTimeZone(
@@ -19,6 +21,12 @@ export function GameHeader(props: GameHeaderProps) {
               "EEE '•' h:mm a",
             )}
       </p>
+      {date !== null && hasGameStarted && (
+        <span className="bg-muted text-muted-foreground flex items-center gap-1 rounded-md px-2 py-1 text-xs">
+          <Lock className="size-3" />
+          Picks Closed
+        </span>
+      )}
     </div>
   );
 }
