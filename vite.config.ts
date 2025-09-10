cf70238df34cf0af3c965d543e311ba2e1a6de5d
Jackson Blankenship <jacksblan@gmail.com>
2025-09-10 17:08:59 -0500

    correct deprecated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
     react(),
     tailwindcss(),
   ],
