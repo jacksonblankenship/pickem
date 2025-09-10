6b837e8c952f2933e2d837a16eaaa5f18129b0a2
Jackson Blankenship <jacksblan@gmail.com>
2025-09-10 18:04:49 -0500

    feat: minor ui tweaks (#56)

--- src/components/game-card/game-header.tsx
+++ src/components/game-card/game-header.tsx
@@ -1,3 +1,4 @@
+import { useHasGameStarted } from '@/hooks/use-has-game-started';
 import { PropsWithClassName } from '@/lib/types';
 import { cn } from '@/lib/utils';
 import { formatInTimeZone } from 'date-fns-tz';
@@ -7,10 +8,12 @@ type GameHeaderProps = PropsWithClassName;
 
 export function GameHeader(props: GameHeaderProps) {
   const { date } = useGameCardContext();
+  const hasGameStarted = useHasGameStarted(date);
 
   return (
-    <div className={cn('flex items-center justify-between', props.className)}>
-      <p className="text-xs text-slate-600">
+    <div
+      className={cn('flex h-6 items-center justify-between', props.className)}>
+      <span className="flex items-center gap-1 text-xs text-slate-600">
         {date === null
           ? 'TBD'
           : formatInTimeZone(
@@ -18,7 +21,12 @@ export function GameHeader(props: GameHeaderProps) {
               Intl.DateTimeFormat().resolvedOptions().timeZone,
               "EEE '•' h:mm a",
             )}
-      </p>
+      </span>
+      {date !== null && hasGameStarted && (
+        <span className="bg-muted text-muted-foreground flex items-center gap-1 rounded-sm px-2 py-1 text-xs">
+          Picks Closed
+        </span>
+      )}
     </div>
   );
 }
