0475a8bdea03989935737744a2f6ba354a6501e7
Jackson Blankenship <jacksblan@gmail.com>
2025-09-09 19:07:15 -0500

    add context guards

--- src/context/week-slate-context.ts
+++ src/context/week-slate-context.ts
@@ -4,5 +4,13 @@ import { createContext, useContext } from 'react';
 export const WeekSlateContext = createContext<WeekSlateData>(null!);
 
 export function useWeekSlateContext() {
-  return useContext(WeekSlateContext);
+  const context = useContext(WeekSlateContext);
+
+  if (!context) {
+    throw new Error(
+      'useWeekSlateContext must be used within a WeekSlateProvider',
+    );
+  }
+
+  return context;
 }
