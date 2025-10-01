862202642884cfe3f1a072d926fa8d4791decd5a
Jackson Blankenship <jacksblan@gmail.com>
2025-10-01 09:12:44 -0500

    configure supabase for vercel edge

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
+import { SupabaseClient } from '@supabase/supabase-js';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClientProvider } from './providers/query-client-provider';
 import { RouterProvider } from './providers/router-provider';
 import { SessionProvider } from './providers/session-provider';
+import { Database } from './supabase/types';
 
 import './index.css';
+
+declare global {
+  var _supabase: SupabaseClient<Database> | undefined;
+}
 
 const rootElement = document.getElementById('root')!;
 if (!rootElement.innerHTML) {
--- src/supabase/index.ts
+++ src/supabase/index.ts
@@ -2,9 +2,12 @@ import { env } from '@/lib/env';
 import { createClient } from '@supabase/supabase-js';
 import { Database } from './types';
 
-export const supabase = createClient<Database>(
-  env.VITE_SUPABASE_URL,
-  env.VITE_SUPABASE_ANON_KEY,
-);
+export const supabase =
+  globalThis._supabase ??
+  createClient<Database>(env.VITE_SUPABASE_URL, env.VITE_SUPABASE_ANON_KEY);
+
+if (env.VITE_VERCEL_ENV !== 'development') {
+  globalThis._supabase = supabase;
+}
 
 export type SupabaseClient = typeof supabase;
