ba5b6d8c6ba432e7777247c749baa129cedab001
Jackson Blankenship <jacksblan@gmail.com>
2025-10-01 09:32:24 -0500

    add scores to game cards

--- src/components/game-card/teams.tsx
+++ src/components/game-card/teams.tsx
@@ -12,30 +12,55 @@ export function Teams(props: TeamsProps) {
     homeTeamAbbr,
     homeTeamLogo,
     neutralSite,
+    gameStatus,
+    awayTeamScore,
+    homeTeamScore,
   } = useGameCardContext();
 
   return (
-    <div className={cn('grid grid-cols-3 items-center gap-3', props.className)}>
+    <div
+      className={cn(
+        'grid grid-cols-[1fr_auto_1fr] items-center gap-3',
+        props.className,
+      )}>
       <div className="flex min-w-0 flex-row items-center gap-2">
         <Avatar className="h-8 w-8 ring-1 ring-slate-200">
           <AvatarImage src={awayTeamLogo} alt={`${awayTeamAbbr} logo`} />
           <AvatarFallback>{awayTeamAbbr}</AvatarFallback>
         </Avatar>
-        <div className="min-w-0">
+        <div className="min-w-8">
           <div className="text-sm font-semibold tracking-wide text-slate-800">
             {awayTeamAbbr}
           </div>
         </div>
       </div>
-      <span className="text-muted-foreground text-center text-xs font-medium select-none">
-        {neutralSite ? 'vs.' : '@'}
-      </span>
+      {gameStatus === 'completed' ? (
+        <div className="text-muted-foreground grid grid-cols-3 gap-2 text-center text-xs">
+          <span
+            className={cn(
+              awayTeamScore > homeTeamScore && 'text-foreground font-medium',
+            )}>
+            {awayTeamScore}
+          </span>
+          <span className="uppercase">final</span>
+          <span
+            className={cn(
+              homeTeamScore > awayTeamScore && 'text-foreground font-medium',
+            )}>
+            {homeTeamScore}
+          </span>
+        </div>
+      ) : (
+        <span className="text-muted-foreground text-center text-xs font-medium select-none">
+          {neutralSite ? 'vs.' : '@'}
+        </span>
+      )}
       <div className="flex min-w-0 flex-row-reverse items-center gap-2 text-right">
         <Avatar className="h-8 w-8 ring-1 ring-slate-200">
           <AvatarImage src={homeTeamLogo} alt={`${homeTeamAbbr} logo`} />
           <AvatarFallback>{homeTeamAbbr}</AvatarFallback>
         </Avatar>
-        <div className="min-w-0">
+        <div className="min-w-8">
           <div className="text-sm font-semibold tracking-wide text-slate-800">
             {homeTeamAbbr}
           </div>
